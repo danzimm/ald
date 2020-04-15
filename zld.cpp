5819f42b4cc1d2a18a08d9d7b9333e3307a17ec1
Dan Zimmerman <daniel.zimmerman@me.com>
2020-04-15 11:57:25 -0400

    Implement loading mach-o object files

--- zld.cpp
+++ zld.cpp
@@ -2,16 +2,23 @@
 
 #include "zld.h"
 
+#include "llvm/Object/MachO.h"
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/WithColor.h"
 
 using namespace llvm;
+using namespace llvm::object;
 using namespace llvm::zld;
 
 static cl::OptionCategory ZldCat("zld Options");
 
 cl::opt<bool> zld::Dummy("dummy", cl::desc("Dummy arg to sanity check cli"),
                          cl::cat(ZldCat));
+
+static cl::list<std::string> InputFilenames(cl::Positional,
+                                            cl::desc("<input object files>"),
+                                            cl::ZeroOrMore, cl::cat(ZldCat));
+
 static cl::extrahelp
     HelpResponse("\nPass @FILE as argument to read options from FILE.\n");
 
@@ -27,15 +34,42 @@ void reportWarning(Twine Message, StringRef File) {
       << "'" << File << "': " << Message << "\n";
   errs().flush();
 }
+#endif
 
 LLVM_ATTRIBUTE_NORETURN void reportError(StringRef File, Twine Message) {
   WithColor::error(errs(), ToolName) << "'" << File << "': " << Message << "\n";
   exit(1);
 }
-#endif
 
-static void reportCmdLineWarning(Twine Message) {
-  WithColor::warning(errs(), ToolName) << Message << "\n";
+LLVM_ATTRIBUTE_NORETURN void reportError(Error E, StringRef FileName,
+                                         StringRef ArchiveName = "",
+                                         StringRef ArchitectureName = "") {
+  assert(E);
+  WithColor::error(errs(), ToolName);
+  if (ArchiveName != "")
+    errs() << ArchiveName << "(" << FileName << ")";
+  else
+    errs() << "'" << FileName << "'";
+  if (!ArchitectureName.empty())
+    errs() << " (for architecture " << ArchitectureName << ")";
+  std::string Buf;
+  raw_string_ostream OS(Buf);
+  logAllUnhandledErrors(std::move(E), OS);
+  OS.flush();
+  errs() << ": " << Buf;
+  exit(1);
+}
+
+template <typename T, typename... Ts>
+T unwrapOrError(Expected<T> EO, Ts &&... Args) {
+  if (EO) {
+    return std::move(*EO);
+  }
+  reportError(EO.takeError(), std::forward<Ts>(Args)...);
+}
+
+static void reportStatus(Twine Message) {
+  WithColor::note(errs(), ToolName) << Message << "\n";
 }
 
 #if 0
@@ -44,6 +78,40 @@ LLVM_ATTRIBUTE_NORETURN static void reportCmdLineError(Twine Message) {
   exit(1);
 }
 #endif
+
+class Context {
+public:
+  Context() {}
+
+  void LoadBins(const std::vector<std::string> &Filenames) {
+    reportStatus("Loading binaries...");
+    Bins.reserve(Filenames.size());
+    ObjectFiles.reserve(Filenames.size());
+    llvm::for_each(Filenames,
+                   [this](StringRef Filename) { this->LoadBin(Filename); });
+  }
+
+private:
+  void LoadBin(StringRef Filename) {
+    OwningBinary<Binary> OBinary =
+        unwrapOrError(createBinary(Filename), Filename);
+    Binary &Binary = *OBinary.getBinary();
+    if (ObjectFile *O = dyn_cast<ObjectFile>(&Binary)) {
+      if (MachOObjectFile *MachO = dyn_cast<MachOObjectFile>(O)) {
+        ObjectFiles.push_back(MachO);
+      } else {
+        reportError(Filename, "not a mach-o object file");
+      }
+    } else {
+      reportError(Filename, "not an object file");
+    }
+    Bins.emplace_back(std::move(OBinary));
+    reportStatus("  Loaded " + Filename);
+  }
+
+  std::vector<OwningBinary<Binary>> Bins;
+  std::vector<MachOObjectFile *> ObjectFiles;
+};
 
 } // namespace
 
@@ -60,13 +128,21 @@ int main(int argc, char **argv) {
                               /*EnvVar=*/nullptr,
                               /*LongOptionsUseDoubleDash=*/true);
 
+  // Defaults to a.out if no filenames specified.
+  if (InputFilenames.empty()) {
+    InputFilenames.push_back("a.out");
+  }
+
   ToolName = argv[0];
 
   if (Dummy) {
-    reportCmdLineWarning("Passed dummy");
+    reportStatus("Passed dummy");
   }
 
-  reportCmdLineWarning("Successfully started up");
+  Context Ctx;
+  Ctx.LoadBins(InputFilenames);
+
+  reportStatus("Successfully started up");
 
   return EXIT_SUCCESS;
 }
